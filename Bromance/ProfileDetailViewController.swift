0201f554ce03c414cf5e83a573b9146cc8e38ffc
AnimeMei <adrian.z.mei@gmail.com>
2017-01-08 16:59:26 -0800

    able to dislpay contacted user info and linked to chat VC

--- Bromance/ProfileDetailViewController.swift
+++ Bromance/ProfileDetailViewController.swift
@@ -12,8 +12,9 @@ import FirebaseDatabase
 import FBSDKCoreKit
 import TwitterKit
 import GoogleSignIn
+import MessageUI
 
-class ProfileDetailViewController: UITableViewController {
+class ProfileDetailViewController: UITableViewController, MFMailComposeViewControllerDelegate {
 
     //store the device id
     let deviceID = UIDevice.current.identifierForVendor?.uuidString
@@ -45,13 +46,70 @@ class ProfileDetailViewController: UITableViewController {
 //        //sign the user out of the twitter
 //        let client = TWTRAPIClient.withCurrentUser()
 //        Twitter.sharedInstance().sessionStore.logOutUserID(client.userID!)
-
-
-        
         
         print("User logged out successfully")
 
     }
+    
+    @IBAction func onReportProblem(_ sender: Any) {
+        let mailComposeViewController = issueConfiguredMailComposeViewController()
+        if MFMailComposeViewController.canSendMail() {
+            self.present(mailComposeViewController, animated: true, completion: nil)
+        } else {
+            self.showSendMailErrorAlert()
+        }
+    }
+    
+    @IBAction func onFeedBack(_ sender: Any) {
+        let mailComposeViewController = feedbackConfiguredMailComposeViewController()
+        if MFMailComposeViewController.canSendMail() {
+            self.present(mailComposeViewController, animated: true, completion: nil)
+        } else {
+            self.showSendMailErrorAlert()
+        }
+    }
+    
+    
+    // *** Send Email ***
+    
+    //report problem email setup
+    func issueConfiguredMailComposeViewController() -> MFMailComposeViewController {
+        let mailComposerVC = MFMailComposeViewController()
+        mailComposerVC.mailComposeDelegate = self // Extremely important to set the --mailComposeDelegate-- property, NOT the --delegate-- property
+        
+        mailComposerVC.setToRecipients(["teamacorne@gmail.com"])
+        mailComposerVC.setSubject("Report a Problem")
+        mailComposerVC.setMessageBody("I am having the following issue(s): ", isHTML: false)
+        
+        return mailComposerVC
+    }
+    
+    //feedback email setup
+    func feedbackConfiguredMailComposeViewController() -> MFMailComposeViewController {
+        let mailComposerVC = MFMailComposeViewController()
+        mailComposerVC.mailComposeDelegate = self // Extremely important to set the --mailComposeDelegate-- property, NOT the --delegate-- property
+        
+        mailComposerVC.setToRecipients(["teamacorne@gmail.com"])
+        mailComposerVC.setSubject("Feedback")
+        mailComposerVC.setMessageBody("Let us know what you enjoy the most about Bromance", isHTML: false)
+        
+        return mailComposerVC
+    }
+    
+    
+    func showSendMailErrorAlert() {
+        let alert = UIAlertController(title: "Could Not Send Email!", message:"Your device could not send e-mail.  Please check e-mail configuration and try again.", preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "OK", style: .default) { _ in })
+        self.present(alert, animated: true){}
+    }
+    
+    // MARK: MFMailComposeViewControllerDelegate Method
+    private func mailComposeController(controller: MFMailComposeViewController, didFinishWithResult result: MFMailComposeResult, error: NSError?) {
+        controller.dismiss(animated: true, completion: nil)
+    }
+    // *** send meial end ***
+
+    
 
 
 }
